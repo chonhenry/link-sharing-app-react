8f4499774a391f280c045eb91b2a81464938647b
chonhenry <chonhenry@gmail.com>
2023-11-09 13:37:55 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
 import "./App.css";
+import { Session } from "@supabase/supabase-js";
+import Auth from "./pages/Auth";
+import Account from "./pages/Account";
 
 function App() {
-  return <div>hello world</div>;
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return (
+    <div className="">
+      {!session ? (
+        <Auth />
+      ) : (
+        <Account key={session.user.id} session={session} />
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      purple: "#633cff",
+      "hover-purple": "#beadff",
+      "light-purple": "#efebff",
+      "dark-grey": "#333333",
+      grey: "#737373",
+      borders: "#d9d9d9",
+      "light-grey": "#fafafa",
+      white: "#ffffff",
+      red: "#ff3939",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
